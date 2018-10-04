c3ce49cdd049422fe11624582a29fe02a899fa52
Phil Hartley <philh725@gmail.com>
2018-10-04 20:34:20 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,9 +6,6 @@ function updatePlaylist(object, string, title) {
 
 function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName]
-  console.log(playlist)
+  return playlist
 }
 
-var test = {'testname': 'title', 'phil':'song'}
-
-console.log(removeFromPlaylist(test, 'testname'))
