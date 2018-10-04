63264f1b1f4348294074954f474a7f9ae061946b
Phil Hartley <philh725@gmail.com>
2018-10-04 20:22:21 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = {'artist name': []};
 
 function updatePlaylist(object, string, title) {
-  return Object.assign(object, {string:title})
+  return Object.assign(object, {[string]:title})
 }
